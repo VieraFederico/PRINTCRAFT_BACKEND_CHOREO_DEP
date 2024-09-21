ade0f7cf69ac168813b6e986e4aabd2083189a06
Federico Viera <fedviera@gmail.com>
2024-09-21 17:40:48 -0300

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("seller/", views.SellerCreateView.as_view(), name="seller_create"),
+    path("user/data/", views.ReturnUserDataView.as_view(), name="user_data"),
+
 ]
