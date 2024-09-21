1ac3f4ed2cf209424dd27818069057da3a16feab
Federico Viera <fedviera@gmail.com>
2024-09-21 16:49:17 -0300

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,7 +5,7 @@ from .models import Seller,Product,Order
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "first_name", "last_name","password"]
+        fields = ["id", "first_name", "last_name", "email","password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
