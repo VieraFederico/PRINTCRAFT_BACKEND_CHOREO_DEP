e90eea08e4181d0f44e1cfe10d7d361d6719e4bd
Federico Viera <fedviera@gmail.com>
2024-09-30 19:45:52 -0300

    obi

--- backend/api/services/supabase_client.py
+++ backend/api/services/supabase_client.py
@@ -1,13 +1,13 @@
 from supabase import create_client, Client
 import os
-
+"""
 url = "https://vvvlpyyvmavjdmfrkqvw.supabase.co"
 service_key = os.getenv("DB_KEY")
 
 supabase: Client = create_client(url, service_key)
-
+"""
 def upload_file_to_supabase(file, bucket_name, file_name):
-    response = supabase.storage.from_(bucket_name).upload(file_name, file)
+    """response = supabase.storage.from_(bucket_name).upload(file_name, file)
     if response.get('error'):
-        raise Exception("Error uploading file to Supabase Storage")
-    return response.get('data')['path']
+        raise Exception("Error uploading file to Supabase Storage")"""
+    return "https://es.wikipedia.org/wiki/Obi-Wan_Kenobi"
