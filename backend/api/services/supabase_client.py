53f84a5575cb33539e114881d333f4048a48af6c
Federico Viera <fedviera@gmail.com>
2024-09-30 21:47:33 -0300

    a

--- backend/api/services/supabase_client.py
+++ backend/api/services/supabase_client.py
@@ -1,11 +1,11 @@
 from supabase import create_client, Client
 import os
-"""
-url = "https://vvvlpyyvmavjdmfrkqvw.supabase.co"
-service_key = os.getenv("DB_KEY")
 
-supabase: Client = create_client(url, service_key)
-"""
+url: str = "https://vvvlpyyvmavjdmfrkqvw.supabase.co"
+key: str = os.environ.get("DB_KEY")
+supabase: Client = create_client(url, key)
+
+
 def upload_file_to_supabase(file, bucket_name, file_name):
     """response = supabase.storage.from_(bucket_name).upload(file_name, file)
     if response.get('error'):
