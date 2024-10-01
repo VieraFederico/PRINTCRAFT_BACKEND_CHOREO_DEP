25217b4f38ddbec03150454e5785ecc6d74f5414
mrossisei <mrossiseifert@itba.edu.ar>
2024-10-01 20:08:00 -0300

    Debería funcionar: testeado con curl

--- backend/api/views.py
+++ backend/api/views.py
@@ -123,7 +123,10 @@ class UserOrderListView(generics.ListAPIView):
 ###############
 #### FILES ####
 ###############
+
+"""
 class FileUploadView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         # file = request.FILES['file']
         file = request.FILES.get('file')
@@ -139,6 +142,27 @@ class FileUploadView(APIView):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_201_CREATED)
             # return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+"""
+
+class FileUploadView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        # file = request.FILES.get('file')
+        file = request.FILES['file']
+        if not file:
+            return Response({"error": "No file provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+        file_name = "matiasferreroelcolorado2"
+        bucket_name = 'images'
+
+        try:
+            # Read the file content
+            file_content = file.read()
+            file_url = upload_file_to_supabase(file_content, bucket_name, file_name)
+            return Response({"url": file_url}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class CreateCheckoutPreferenceView(APIView):
     permission_classes = [IsAuthenticated]
