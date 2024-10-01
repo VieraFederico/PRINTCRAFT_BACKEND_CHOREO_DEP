59cc975438c052220c5bf7cab57e42d3a05e425e
mrossisei <mrossiseifert@itba.edu.ar>
2024-10-01 09:32:59 -0300

    upload_file_to_supabase funcionando adecuadamente

--- backend/api/services/supabase_client.py
+++ backend/api/services/supabase_client.py
@@ -7,20 +7,21 @@ supabase: Client = create_client(url, key)
 
 def upload_file_to_supabase(file, bucket_name, file_name):
     # response = supabase.storage.from_("images").upload(file_name, file)
-    with open(file_name, 'rb') as f:
-        response = supabase.storage.from_("images").upload(file=f, path="probando_imagenes_obiwan", file_options={"content-type": "image/jpeg"})
+    response = supabase.storage.from_(bucket_name).upload(file=file, path=file_name, file_options={"content-type": "image/jpeg"})
 
     if response.is_error:
         raise Exception(response.error)
-    return "obi_wan_kenobi"
+    return file_name
 
 def test_upload_file():
     # Crear un archivo de prueba
     test_file_name = "images.jpg"
 
     # Subir el archivo de prueba
+
     try:
-        file_url = upload_file_to_supabase(test_file_name, "test_bucket", test_file_name)
+        with open(test_file_name, 'rb') as f:
+            file_url = upload_file_to_supabase(f, "images", "darthmaul")
         print(f"File uploaded successfully: {file_url}")
     except Exception as e:
         print(f"Error: {e}")
