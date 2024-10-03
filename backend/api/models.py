d313bf6195e0f5ec830bf5af34319cec2dc7c491
mrossisei <mrossiseifert@itba.edu.ar>
2024-10-03 18:04:55 -0300

    profile_picture para vendedor

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,6 +6,7 @@ class Seller(models.Model):
     address = models.CharField(max_length=255, null=False)
     store_name = models.CharField(max_length=255, unique=True, null=False)
     description = models.TextField()
+    profile_picture = models.URLField(max_length=200, null=True, blank=True)
 
     # rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
     # total_sales = models.IntegerField(default=0)
