4e8bf2a84df0852a30993e0afd032ec374844710
mrossisei <mrossiseifert@itba.edu.ar>
2024-10-07 14:29:35 -0300

    modelo y migraciones nuevas

--- backend/api/models.py
+++ backend/api/models.py
@@ -72,12 +72,19 @@ class PrintRequest(models.Model):
                                        ("En proceso", "En proceso"), ("Realizada", "Realizada")]
                               )
 
-"""
+class DesignRequestImage(models.Model):
+    image_url = models.URLField(max_length=200, primary_key=True)
+
+    def __str__(self):
+        return f"Image for {self.image_url}"
+
+
 class DesignRequest(models.Model):
     requestID = models.AutoField(primary_key=True)
     userID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     sellerID = models.ForeignKey(Seller, on_delete=models.SET_NULL, null=True)
     description = models.TextField()
+    design_images = models.ManyToManyField('DesignRequestImage')
     quantity = models.IntegerField(null=False)
     material = models.CharField(max_length=255, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
@@ -86,7 +93,7 @@ class DesignRequest(models.Model):
                                        ("Cotizada", "Cotizada"), ("Cancelada", "Cancelada"),
                                        ("En proceso", "En proceso"), ("Realizada", "Realizada")]
                               )
-"""
+
 # PLA, PETG, ABS, Nailon
 
 class Order(models.Model):
