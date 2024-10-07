b001796bbf9833ccfca684b6e58f7093bfc4e5e1
Federico Viera <fedviera@gmail.com>
2024-10-07 16:14:02 -0300

    Added Email Again

--- backend/api/views.py
+++ backend/api/views.py
@@ -440,12 +440,13 @@ class CreatePaymentView(APIView):
     def post(self, request):
         product_id = request.data.get("product_id")
         quantity = request.data.get("quantity")
+        email = request.data.get("email")
 
         product_selected = Product.objects.get(code=product_id)
         transaction_amount = Decimal(product_selected.price) * int(quantity)
         if int(quantity) > product_selected.stock:
             return Response({"error": "Missing required fields."}, status=status.HTTP_400_BAD_REQUEST)
-        if not all([product_id, quantity, transaction_amount]):
+        if not all([product_id, quantity, transaction_amount, email]):
             return Response({"error": "Missing required fields."}, status=status.HTTP_400_BAD_REQUEST)
         access_token = str(settings.MERCADOPAGO_ACCESS_TOKEN)
         if not access_token:
@@ -461,6 +462,9 @@ class CreatePaymentView(APIView):
                     "unit_price": float(transaction_amount)
                 }
             ],
+            "payer": {
+                "email": email
+            },
             "back_urls": {
                 "success": "https://3dcapybara.vercel.app/success",
                 "failure": "https://3dcapybara.vercel.app/failure",
