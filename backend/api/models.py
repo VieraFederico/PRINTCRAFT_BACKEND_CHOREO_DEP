511b033c5dcaa185118160dbe330df36c2d50e2f
mrossisei <mrossiseifert@itba.edu.ar>
2024-10-16 13:49:35 -0300

    actualización modelos

--- backend/api/models.py
+++ backend/api/models.py
@@ -138,6 +138,7 @@ class PrintReverseAuction(models.Model):
     stl_file_url = models.URLField(max_length=200, null=False)
     status = models.CharField(max_length=255, null=False, default="Open",
                               choices=[("Open", "Open"), ("Closed", "Closed")])
+    response_count = models.IntegerField(null=False, default=0)
     accepted_response = models.OneToOneField('PrintReverseAuctionResponse', on_delete=models.SET_NULL, null=True, blank=True, related_name='accepted_auction')
 
 
