8509e4ab122541c2776883148e00bcdd9057c535
mrossisei <mrossiseifert@itba.edu.ar>
2024-11-30 18:52:40 -0300

    desactivar páginas donde no tienen que ir

--- backend/api/views.py
+++ backend/api/views.py
@@ -466,6 +466,7 @@ class UserPrintRequestListView(APIView):
 class SellerPrintRequestListView(generics.ListAPIView):
     serializer_class = PrintRequestSerializer
     permission_classes = [IsSeller]
+    pagination_class = None
 
     def get_queryset(self):
         seller = self.request.user.seller
@@ -695,6 +696,7 @@ class UserPrintRequestListView(APIView):
 class SellerDesignRequestListView(generics.ListAPIView):
     serializer_class = DesignRequestSerializer
     permission_classes = [IsSeller]
+    pagination_class = None
     # permission_classes = [AllowAny] # TOD CAMBIAR
 
     def get_queryset(self):
@@ -894,6 +896,7 @@ class PrintReverseAuctionCreateView(generics.CreateAPIView):
 class UserPrintReverseAuctionListView(generics.ListAPIView):
     serializer_class = PrintReverseAuctionSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = None
     # permission_classes = [AllowAny] # TOD CAMBIAR
 
     def get_queryset(self):
@@ -904,6 +907,7 @@ class UserPrintReverseAuctionListView(generics.ListAPIView):
 class OpenPrintReverseAuctionListView(generics.ListAPIView):
     serializer_class = PrintReverseAuctionSerializer
     permission_classes = [AllowAny]
+    pagination_class = None
 
     def get_queryset(self):
         return PrintReverseAuction.objects.filter(status="Open")
@@ -1144,6 +1148,7 @@ class DesignReverseAuctionCreateView(generics.CreateAPIView):
 class UserDesignReverseAuctionListView(generics.ListAPIView):
     serializer_class = DesignReverseAuctionSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = None
     # permission_classes = [AllowAny] # TODO CAMBIAR
 
     def get_queryset(self):
@@ -1154,6 +1159,7 @@ class UserDesignReverseAuctionListView(generics.ListAPIView):
 class OpenDesignReverseAuctionListView(generics.ListAPIView):
     serializer_class = DesignReverseAuctionSerializer
     permission_classes = [AllowAny]
+    pagination_class = None
 
     def get_queryset(self):
         return DesignReverseAuction.objects.filter(status="Open")
